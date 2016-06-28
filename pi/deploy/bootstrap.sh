b5b835aa1263cbea15e2e0b39149f0f7e3dc6ca8
dchaffey <dchaffey@hortonworks.com>
2016-06-28 10:32:34 -0700

    Fixed ethtool update in bootstrap

--- pi/deploy/bootstrap.sh
+++ pi/deploy/bootstrap.sh
@@ -68,16 +68,16 @@ chmod 0444 ./nifi.tar.gz
 ###
 
 ### OS Update if not run recently
-# if [ $(stat -c %Y /var/cache/apt/) -lt $(date +%s -d "2 days ago") ]; then
-#    log "Running OS update/upgrade/firmware update..."
-#    apt-key update
-#    apt-get update -y
+if [ $(stat -c %Y /var/cache/apt/) -lt $(date +%s -d "2 days ago") ]; then
+    log "Running OS update/upgrade/firmware update..."
+    apt-key update
+    apt-get update -y
 #    apt-get upgrade -y
 #    rpi-update
-#    apt-get autoremove -y
-# else 
-#    log "OS update run recently, skipping..."
-# fi
+    apt-get autoremove -y
+else 
+    log "OS update run recently, skipping..."
+fi
 
 # Install dependencies
 log "Checking software dependencies..."
@@ -115,6 +115,7 @@ fi
 # Getting Overlay
 OVERLAY_URL="${OVERLAY}/${MAC}"
 log "Found Nifi install, now testing Bastion access"
+log "Using Overlay URL constructed from Config and Mac of: ${OVERLAY_URL}"
 # testing if Pi is not registered with bastion
 if ! curl -fIsS --pass $(cat /home/pi/certs/prov-client-passwd) --cacert /home/pi/certs/ca.crt --key /home/pi/certs/client.key --cert /home/pi/certs/client.crt ${OVERLAY_URL} ; then
     # Registering with Overlay
